89adbd1f152eff279ec485abf6ed4786edbac993
Thanatip-iceJ <p.thanatip1@gmail.com>
2024-01-25 23:04:37 +0700

    before db pull

--- src/controllers/account-controller.js
+++ src/controllers/account-controller.js
@@ -1,26 +1,15 @@
 const createError = require("../utils/create-error");
 const prisma = require("../config/prisma");
 
-exports.createAcc = async (req, res, next) => {
-  try {
-    if (!req.user) {
-      next(createError("You're unauthorized.", 401));
-      return;
-    }
-  } catch (err) {
-    next(err);
-  }
-};
-
 exports.getAll = async (req, res, next) => {
   try {
     const myAccounts = await prisma.account.findMany({
       where: {
         userId: +req.user.id,
       },
-      include: {
-        type,
-      },
+      // include: {
+      //   a
+      // },
     });
     res.status(200).json(myAccounts);
   } catch (err) {
